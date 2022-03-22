f39af2e74cd44a93cdea74b7601363fad7cf8910
jacinta254 <jecihjoy@gmail.com>
2022-03-22 21:56:20 +0300

    .

---
+++ src/main/java/com/jecihjoy/playground/SpringbootThymeleafPlayground.java
@@ -0,0 +1,13 @@
+package com.jecihjoy.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafPlayground {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafPlayground.class, args);
+	}
+
+}
---
+++ src/main/java/com/jecihjoy/playground/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.jecihjoy.playground.service;
+
+import java.util.List;
+
+import com.jecihjoy.playground.model.Employee;
+import org.springframework.data.domain.Page;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployeeById(long id);
+
+    void deleteEmployeeById(long id);
+
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+}
